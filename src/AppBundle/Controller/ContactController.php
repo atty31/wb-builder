3120e868e22c183c9a99b6230144cd303cc537d0
Attila Naghi <naghi.attila@gmail.com>
2016-10-29 21:13:47 +0300

    [TASK] - Rendering view for contact controller.

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -9,18 +9,22 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ContactController
+class ContactController extends Controller
 {
     /**
-     * @Route("/contact/")
+     * @Route("/contact/", name="contact")
      */
     public function indexAction()
     {
         $number = mt_rand(0, 100);
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('contact/content.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
