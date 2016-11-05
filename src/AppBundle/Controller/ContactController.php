c62f6a0817225200fe19b5c48f76e3bdadd79a5a
Attila Naghi <naghi.attila@gmail.com>
2016-11-05 20:49:36 +0200

    [TASK] - Added contact form.

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -7,24 +7,36 @@
  */
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ContactController extends Controller
 {
     /**
-     * @Route("/contact/", name="contact")
+     * @Route("/contact/", name="contact").
      */
     public function indexAction()
     {
-        $number = mt_rand(0, 100);
-//        return new Response(
-//            '<html><body>Lucky number: '.$number.'</body></html>'
-//        );
+        $contact = new Contact();
+
+        $form = $this->createFormBuilder($contact)
+            ->add('name', TextType::class)
+            ->add('email', TextType::class)
+            ->add('message', TextareaType::class, array(
+                'attr' => array('class' => 'tinymce'),
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Send Message'))
+            ->getForm();
 
         return $this->render('contact/content.html.twig', array(
-            'number' => $number,
+            'form' => $form->createView(),
         ));
     }
 }
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,70 @@
+<?php
+namespace AppBundle\Entity;
+
+/**
+ * Class Contact
+ * @package AppBundle\Entity
+ */
+class Contact
+{
+    /**
+     * @var $name
+     */
+    protected $name;
+    /**
+     * @var $email
+     */
+    protected $email;
+    /**
+     * @var $message
+     */
+    protected $message;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+}
